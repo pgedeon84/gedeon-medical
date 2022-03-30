0f36265e0cb0e9e0ad9b9989f00e5bc69f619304
Patrice Gedeon <pgedeon84@gmail.com>
2022-03-30 19:47:35 -0400

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="initail-scale=1.0, width=device-width" />
-        <link rel="icon" href="GMC-favicon-logo.svg" />
+        <link rel="icon" type="image/png" href="GMC-favicon-logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
