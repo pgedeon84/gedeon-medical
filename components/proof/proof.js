27291d6665effc9af9910255cadea896a26700d0
Patrice Gedeon <pgedeon84@gmail.com>
2022-10-26 01:35:47 -0400

    added text to proof

--- components/proof/proof.js
+++ components/proof/proof.js
@@ -12,65 +12,83 @@ import devoted from "../../public/images/DevotedHealth.svg";
 
 function Proof() {
   return (
-    <div className={classes.gmc__proof}>
-      <div>
-        <Image
-          src={blue_cross}
-          alt="blue-cross insurance"
-          layout="responsive"
-          priority
-        />
+    <>
+      <p className={classes.gmc__proof_title}>Accepted Insurances</p>
+      <div className={classes.gmc__proof}>
+        <div>
+          <Image
+            src={blue_cross}
+            alt="blue-cross insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={brightHealth}
+            alt="bright health care"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={medicare}
+            alt="medicare insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={mhn}
+            alt="memorial healthcare network"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={medicade}
+            alt="medicade insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={oscar}
+            alt="oscar insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={devoted}
+            alt="devoted health insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={cigna}
+            alt="cigna insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={solis}
+            alt="solis insurance"
+            layout="responsive"
+            priority
+          />
+        </div>
       </div>
-      <div>
-        <Image
-          src={brightHealth}
-          alt="bright health care"
-          layout="responsive"
-          priority
-        />
-      </div>
-      <div>
-        <Image
-          src={medicare}
-          alt="medicare insurance"
-          layout="responsive"
-          priority
-        />
-      </div>
-      <div>
-        <Image
-          src={mhn}
-          alt="memorial healthcare network"
-          layout="responsive"
-          priority
-        />
-      </div>
-      <div>
-        <Image
-          src={medicade}
-          alt="medicade insurance"
-          layout="responsive"
-          priority
-        />
-      </div>
-      <div>
-        <Image src={oscar} alt="oscar insurance" layout="responsive" priority />
-      </div>
-      <div>
-        <Image
-          src={devoted}
-          alt="devoted health insurance"
-          layout="responsive"
-          priority
-        />
-      </div>
-      <div>
-        <Image src={cigna} alt="cigna insurance" layout="responsive" priority />
-      </div>
-      <div>
-        <Image src={solis} alt="solis insurance" layout="responsive" priority />
-      </div>
-    </div>
+    </>
   );
 }
 
