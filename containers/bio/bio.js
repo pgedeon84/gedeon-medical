b204f416eee3e76bdd3cbfcd6778580ecf4a9705
Patrice Gedeon <pgedeon84@gmail.com>
2022-11-01 20:15:14 -0400

    reduced header font sizes

--- containers/bio/bio.js
+++ containers/bio/bio.js
@@ -21,7 +21,7 @@ function Bio() {
           </div>
           <div className={classes.gmc__bio_content_container__content}>
             <h1>Dr. Leon S. Gedeon</h1>
-            <h3>Primary Care - Family Medicine</h3>
+            <p>Primary Care - Family Medicine</p>
             <div className={classes.gmc__bio_content_container__content_rating}>
               <p>Patient Experience Rating -</p>
               <Stars hexColor={"#F6B703"} />
