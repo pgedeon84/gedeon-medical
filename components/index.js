9aa935d2ec2a7132d8523ce2336c737f4c811ad7
Patrice Gedeon <pgedeon84@gmail.com>
2025-04-24 18:59:18 -0400

    added animations to the entire site

--- components/index.js
+++ components/index.js
@@ -3,6 +3,6 @@ export { default as Review } from "./review/review";
 export { default as Card } from "./card/card";
 export { default as Navbar } from "./navbar/navbar";
 export { default as NavbarSpacer } from "./navbar/navbarSpacer";
-export { default as PageTransition } from "./page-transition/PageTransition";
+export { default as PageTransition } from "./page-transition/page-transition";
 export { default as AnimatedSection } from "./ui/animatedSection";
 export { default as AnimatedItem } from "./ui/animatedItem";
