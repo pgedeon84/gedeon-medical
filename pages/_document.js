d3bc61ac12b6704a77d1897e22b515d6b7b9c4b9
Patrice Gedeon <pgedeon84@gmail.com>
2025-04-26 00:50:25 -0400

    testing if FOUC is fixed

--- pages/_document.js
+++ pages/_document.js
@@ -10,18 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="no-js">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Poppins:wght@300;400;500&display=swap"
-            rel="stylesheet"
-            media="print"
-            onLoad="this.media='all'"
-          />
           <style
             dangerouslySetInnerHTML={{
               __html: `
