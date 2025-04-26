59ae0591720e01bdfe3928a03b84bc63b28c2ed6
Patrice Gedeon <pgedeon84@gmail.com>
2025-04-26 01:59:51 -0400

    testing if FOUC is fixed

--- components/page-transition/page-transition.js
+++ components/page-transition/page-transition.js
@@ -9,47 +9,13 @@ const PageTransition = ({ children }) => {
   }, []);
 
   return (
-    // <motion.div
-    //   initial={{ opacity: 0, y: 20 }}
-    //   animate={{
-    //     opacity: 1,
-    //     y: 0,
-    //     transitionEnd: {
-    //       display: "block",
-    //     },
-    //   }}
-    //   exit={{
-    //     opacity: 0,
-    //     y: -20,
-    //     transition: { duration: 0.2 },
-    //   }}
-    //   transition={{
-    //     type: "spring",
-    //     stiffness: 100,
-    //     damping: 20,
-    //     duration: 0.2,
-    //   }}
-    //   onAnimationStart={() => {
-    //     document.body.classList.add("animating");
-    //   }}
-    //   onAnimationComplete={() => {
-    //     document.body.classList.remove("animating", "changing-route");
-    //   }}
-    //   style={{ display: "block" }}
-    // >
-    //   {children}
-    // </motion.div>
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-      onAnimationStart={() => {
-        // Lock scroll during transitions
-        document.documentElement.style.overflow = "hidden";
-      }}
-      onAnimationComplete={() => {
-        document.documentElement.style.overflow = "";
+      initial={{ opacity: 0, y: 8 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -8 }}
+      transition={{
+        duration: 0.3,
+        ease: [0.16, 1, 0.3, 1], // Nice easing curve
       }}
     >
       {children}
